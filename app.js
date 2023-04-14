127553cfc41be795383be820ae44ea71ad266d28
sharjot99 <130633462+sharjot99@users.noreply.github.com>
2023-04-14 10:48:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan")
+const path = require("path")
 const app = express();
 const mongoose = require('mongoose');
 require("./db/conn")
@@ -8,7 +10,12 @@ const cors = require('cors');
 const router = require ("./routes/router")
 const port = process.env.PORT || 8003;
 
+app.use(morgan("dev"))
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.get("*", function(req,res){
+   res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 app.listen(port,()=>{console.log(`Server is Running at ${port}`)}) //string literals use ``,${}//
